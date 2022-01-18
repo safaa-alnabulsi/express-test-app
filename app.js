18b2469229d9ac3886039b512d10a24aecd94176
Safaa Alnabulsi <safaa.alnablsi@gmail.com>
2022-01-18 15:40:48 +0100

    Add initial post endpoints

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser')
+const multer = require('multer') // v1.0.5
+
+app.use(bodyParser.json()) // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+
 let TODOS = ["put on sock", "put on shoe", "tie laces"]
 
 app.get('/',(req,res)=>{
@@ -21,9 +28,15 @@ app.get('/todos/:id', (req, res) => {
         res.status(404).send("Sorry can't find that!")
     }
     else {
-        res.send(TODOS[todo_id])
+        res.send({'title': TODOS[todo_id]})
     }
-    
+})
+
+app.post('/todos', function (req, res) {
+    const todo_text = req.body.title;  
+    const UPDATED_TODOS = TODOS.push(todo_text)  
+    console.log(UPDATED_TODOS)
+    res.send('Got a POST request and updaed TODOS list!')
 })
 
 app.listen(port, () => {
