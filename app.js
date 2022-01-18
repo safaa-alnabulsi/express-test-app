685d98c94ca8849b2f0db98cfdfc30202684cadc
Safaa Alnabulsi <safaa.alnablsi@gmail.com>
2022-01-18 15:05:34 +0100

    Add endpoint get/id

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
 const express = require('express')
 const app = express()
 const port = 3000
+let TODOS = ["put on sock", "put on shoe", "tie laces"]
 
+app.get('/',(req,res)=>{
+    res.redirect('/todos')
+ })
+ 
 app.get('/todo', (req, res) => {
     res.redirect('/todos')
 })
 
 app.get('/todos', (req, res) => {
-    res.send(["put on sock", "put on shoe", "tie laces"])
+    res.send(TODOS)
 })
     
+app.get('/todos/:id', (req, res) => {
+    const todo_id = req.params.id;  
+    if(typeof TODOS[todo_id] === 'undefined') {
+        res.status(404).send("Sorry can't find that!")
+    }
+    else {
+        res.send(TODOS[todo_id])
+    }
+    
+})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
