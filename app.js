41968d39f435360039bd91cd8f6da2a7fbfe7a1e
Safaa Alnabulsi <safaa.alnablsi@gmail.com>
2022-01-18 14:55:15 +0100

    todo, todos get api endpoint

--- app.js
+++ app.js
@@ -2,9 +2,14 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/todo', (req, res) => {
+    res.redirect('/todos')
 })
+
+app.get('/todos', (req, res) => {
+    res.send(["put on sock", "put on shoe", "tie laces"])
+})
+    
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
