668da442870566a17bc58cb99a8bd34d43b23b6c
Safaa Alnabulsi <safaa.alnablsi@gmail.com>
2022-01-18 15:46:54 +0100

    Add intial put endpoint

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.get('/todos', (req, res) => {
 app.get('/todos/:id', (req, res) => {
     const todo_id = req.params.id;  
     if(typeof TODOS[todo_id] === 'undefined') {
-        res.status(404).send("Sorry can't find that!")
+        res.status(404).send("Sorry can't find that todo with given ID!")
     }
     else {
         res.send({'title': TODOS[todo_id]})
@@ -36,8 +36,21 @@ app.post('/todos', function (req, res) {
     const todo_text = req.body.title;  
     const UPDATED_TODOS = TODOS.push(todo_text)  
     console.log(UPDATED_TODOS)
-    res.send('Got a POST request and updaed TODOS list!')
+    res.send('Got a POST request and added a new item to TODOS list!')
 })
+
+app.put('/todos/:id', function (req, res) {
+    const todo_id = req.params.id;  
+    const todo_text = req.body.title;  
+    if(typeof TODOS[todo_id] === 'undefined') {
+        res.status(404).send("Sorry can't find that todo with given ID!")
+    }
+    else {
+        TODOS[todo_id] = todo_text
+        res.send('Got a POST request and updaed TODOS list item!')
+    }
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
